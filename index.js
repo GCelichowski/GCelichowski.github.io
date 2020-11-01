22c4d298dde8f2beeaf66ef37d31b74d48b17eb0
Gavin Celichowski <celic005@umn.edu>
2020-11-01 17:13:35 -0600

    debugging ajax call

---
+++ index.js
@@ -0,0 +1,41 @@
+var postUrl = "https://ycm1r4fi1e.execute-api.us-east-1.amazonaws.com/default/SezzleCalculator?callback=?";
+var getUrl = "https://ycm1r4fi1e.execute-api.us-east-1.amazonaws.com/default/retrievecalculations?callback=?";
+function postCalculation(){
+    var calculation = {
+        "calculation" : document.getElementById("input")
+    };
+
+    $.ajax({
+        url: postUrl,
+        crossDomain : true,
+        contentType : "application/json",
+        dataType: 'json',
+        data : calculation,
+        type: 'POST',
+        success: function(callback) {
+            console.log( 'success' );
+            display(callback.data)
+        }
+    });
+}
+    function display(calculations){
+        var html = "";
+        calculations.array.forEach(element => {
+            html += "<tr><td class='calcInfo'><div class = 'calcBox'>" + JSON.stringify(element, undefined, 2) + "</div></td>";
+        });
+        document.getElementById("calculations").innerHTML = html;
+    }
+
+    function getCalculations() {
+        $.ajax({
+            url: getUrl,
+            crossDomain : true,
+            contentType : "application/json",
+            dataType: 'json',
+            type: 'GET',
+            success: function(callback) {
+                console.log( 'success' );
+                display(callback.data)
+            }
+        });
+    }
